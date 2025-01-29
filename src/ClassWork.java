64951af945b118e27c4847c5e0b5e85accb6997b
Sergii <pavlshns@gmail.com>
2025-01-29 20:43:36 +0200

    Class1

---
+++ src/ClassWork.java
@@ -0,0 +1,51 @@
+public class ClassWork {
+    public static void main(String[] args) {
+
+        System.out.println("Serzh");
+        boolean pershui = true;
+        byte druguj = 120;
+        short tretij = 1500;
+        char chetvertuj = 't';
+        int pjatuj = 8500000;
+        long shostuj = 123456789L;
+        float sjomuj = 75.78524f;
+        double vosmuj = 85.45;
+        String name = "Serzh2";
+        String name2 = "serzh2";
+        System.out.println(name + " "+ name2);
+        System.out.println(name.equals(name2));
+        System.out.println(name.equalsIgnoreCase(name2));
+
+        System.out.println(pershui);
+        System.out.println(druguj);
+        System.out.println(tretij);
+        System.out.println(chetvertuj);
+        System.out.println(pjatuj);
+        System.out.println(shostuj);
+        System.out.println(sjomuj);
+        System.out.println(vosmuj);
+        System.out.println(name);
+
+        int a = 2;
+        int b = 3;
+
+        double db = 1178.2;
+        int db1 = (int) db;
+        System.out.println(db1);
+
+        int db2 = 14850;
+        double db3 = db2;
+        System.out.println(db3);
+
+        int addition = a+b;
+        int subtraction = b - a;
+        int multiplication = a*b;
+        int division = b/a;
+
+        System.out.println(addition);
+        System.out.println(subtraction);
+        System.out.println(multiplication);
+        System.out.println(division);
+        System.out.println(a>b);
+    }
+}
